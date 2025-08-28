ab2a89247818f8e069b961a4c4193476af860eb8
Naufal Alauddin Hilmi <naufal.alauddin@ui.ac.id>
2025-08-28 12:56:19 +0700

    migrate browser action

--- src/Browser/BrowserAction.js
+++ src/Browser/BrowserAction.js
@@ -1,3 +1,3 @@
-"use strict"
+"use strict";
 
-exports.onClicked = browser.browserAction.onClicked;
+export const onClicked = browser.browserAction.onClicked;
--- src/Browser/BrowserAction.purs
+++ src/Browser/BrowserAction.purs
@@ -1,3 +1,4 @@
+-- | [browserAction](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/browserAction)
 module Browser.BrowserAction where
 
 import Browser.Event (SimpleEvent)
