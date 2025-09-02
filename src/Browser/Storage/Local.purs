529bd8f403eabe80c3c0cde5cacdbd93c804d2f7
Naufal Alauddin Hilmi <naufal.alauddin@ui.ac.id>
2025-09-02 16:20:05 +0700

    add documentation

--- src/Browser/Storage/Local.purs
+++ src/Browser/Storage/Local.purs
@@ -1,4 +1,10 @@
--- | [browser.storage.local]()
+-- | [browser.storage.local](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/local)
+-- |
+-- | This a low level binding with minimal safety. Because the storage can store arbitrary json, this binding uses
+-- | [`Json`](https://pursuit.purescript.org/packages/purescript-argonaut-core/7.0.0/docs/Data.Argonaut.Core#t:Json) from
+-- | [`argonaut-core`](https://pursuit.purescript.org/packages/purescript-argonaut-core/7.0.0).
+-- | Encoding/decoding to/from [`Json`](https://pursuit.purescript.org/packages/purescript-argonaut-core/7.0.0/docs/Data.Argonaut.Core#t:Json)
+-- | is left to the user of this binding. 
 module Browser.Storage.Local
   ( clear
   , getByKey
@@ -20,9 +26,16 @@ foreign import _getByKeyImpl :: EffectFn1 String (Promise Json)
 foreign import _getByArrayImpl :: EffectFn1 (Array String) (Promise Json)
 foreign import _getByRecordImpl :: forall r. EffectFn1 { | r } (Promise { | r })
 
+-- | Retrieve an item stored in the local storage by key. If there isn't an
+-- | associated value to that key, it will return an empty object (object with no
+-- | field) if exist, return an Objet with the key as the field and stored value
+-- | as its value.
 getByKey :: String -> Aff Json
 getByKey = Promise.toAffE <<< runEffectFn1 _getByKeyImpl
 
+-- | Similar to `getByKey` but use record instead. The record's fields are used as
+-- | the key. If there isn't a value associated with the key, the original value
+-- | from the arguments will be used. 
 getWithDefault :: forall r. Homogeneous r Json => { | r } -> Aff { | r }
 getWithDefault = Promise.toAffE <<< runEffectFn1 _getByRecordImpl
 
